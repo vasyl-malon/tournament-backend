e911ea1b1edf3641785180df564f218bc25ba74d
v.malon-c <v.malon-c@tamkeentech.sa>
2026-07-14 09:44:46 +0300

    fix: update cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-app.enableCors({
-    origin: 'http://localhost:3000', // URL твого фронтенду
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://tournament-frontend-d48p.vercel.app'],
     credentials: true,
   });
-
   await app.listen(process.env.PORT ?? 8080, () => console.log('Server started'));
 }
 bootstrap();
